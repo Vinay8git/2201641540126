5af490dbd6621007d6a90089b53540179ff22a3b
Vinay8git <vinayyd277@gmail.com>
2025-09-08 14:03:23 +0530

    Set Up the Primary Endpoints

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config('./.env'); //Load Environment Variables from .env file.
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//POST Method
+app.use("/api/create", shortUrl);
+
+//GET Method
+app.get("/:id", redirectFromShortUrl);
+
+app.listen(3000, ()=> {
+    console.log("Server is running on port 3000");
+})
+
+
+//Two HTTP Methods are used in this code:
+//GET : For Redirection
+//POST : For Creating Short URLs
